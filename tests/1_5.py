24fb4e7727defe304f10e6f187c7be194ecb93d8
ping <everping@outlook.com>
2017-03-02 10:54:56 +0700

    update test for challenge 5

--- tests/1_5.py
+++ tests/1_5.py
@@ -1,8 +1,10 @@
 from challenges.set1.challenge5.chall5 import repeating_key_xor
 
-plain_text = """Burning 'em, if you ain't quick and nimble
+if __name__ == '__main__':
+    expected = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d6334' \
+               '3c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
+    plain_text = """Burning 'em, if you ain't quick and nimble
 I go crazy when I hear a cymbal"""
 
-key = 'ICE'
-
-print repeating_key_xor(plain_text, key)
+    key = 'ICE'
+    print repeating_key_xor(plain_text, key).strip() == expected.strip()
